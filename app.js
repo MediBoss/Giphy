a4509e4d94b8879fa281d9dba8765e93a4eff65a
MediBoss <mediassumani49@gmail.com>
2018-08-25 16:48:47 -0700

    added handlebar library and created views

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
   // Setting up express environment
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 
-app.get('/hello-squirrel'. function(req, res){
-  res.send('Hello Mr. Squirell')
-});
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port localhost:3000!');
+  console.log('Giphy listening on port localhost:3000!');
 });
