0df85607d17f1d23d738341a06671f9c9eff36e5
MediBoss <mediassumani49@gmail.com>
2018-08-25 16:17:28 -0700

    setting up express

---
+++ app.js
@@ -0,0 +1,7 @@
+  // Setting up express environment
+var express = require('express');
+var app = express()
+
+app.listen(3000, function () {
+  console.log('Giph Search listening on port localhost:3000!');
+})
