83e7b6bbfb2724df15863072fb98bc77f24ba678
MediBoss <mediassumani49@gmail.com>
2018-08-25 23:31:01 -0700

    making request to giphy api

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-  // Setting up express environment
+
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
+var http = require('http')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.get('/hello-gif', function(req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello gif', {gifUrl: gifUrl})
-})
+app.get('/', function (req, res) {
+  console.log(req.query.term)
+  var queryString = "Funny Cat";
+    // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  var term = encodeURIComponent(queryString);
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaT0xFJmzC'
+
+  http.get(url, function(response) {
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      body+= d;
+    });
+    response.on('end', function() {
+      //WHEN DATA IS FULY RECEIVED, PARSE INTO JSON
+      var parsedData = JSON.parse(body);
+      res.render('home', {gifs: parsedData})
+    });
+  });
+});
 
 app.listen(3000, function () {
   console.log('Giphy listening on port localhost:3000!');
