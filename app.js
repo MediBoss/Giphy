16c0390acb37a17d2fa4af39881aca85b43fdb73
MediBoss <mediassumani49@gmail.com>
2018-08-26 16:36:48 -0700

    added giphy-api wrapper for refactoring

--- app.js
+++ app.js
@@ -2,33 +2,17 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
-var http = require('http')
+var giphy = require('giphy-api')();
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
 app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term;
-    // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-  var term = encodeURIComponent(queryString);
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaT0xFJmzC'
-
-  http.get(url, function(response) {
-    response.setEncoding('utf8');
-
-    var body = '';
-
-    response.on('data', function(d) {
-      body+= d;
-    });
-    response.on('end', function() {
-      //WHEN DATA IS FULY RECEIVED, PARSE INTO JSON
-      var parsedData = JSON.parse(body);
-      res.render('home', {gifs: parsedData})
-    });
-  });
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  })
 });
 
 app.listen(3000, function () {
