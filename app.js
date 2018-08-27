bb2809fcc7ebf7250e84c4958da22e75d0ce16a5
MediBoss <mediassumani49@gmail.com>
2018-08-26 17:25:59 -0700

    wrapping up

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 
 var express = require('express');
-var app = express();
-var exphbs = require('express-handlebars');
+var exphbs  = require('express-handlebars');
+var http = require('http');
 var giphy = require('giphy-api')();
 
+var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  giphy.search(req.query.term, function (err, response) {
-    res.render('home', {gifs: response.data})
-  })
-});
+  var queryString = req.query.term;
+  var term = encodeURIComponent(queryString);
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+
+  http.get(url, function(response) {
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // Continuously update stream with data
+      body += d;
+    });
+
+    response.on('end', function() {
+      // Data reception is done, do whatever with it!
+      var parsed = JSON.parse(body);
+      res.render('home', {gifs: parsed.data})
+    });
+  });
+
+})
 
 app.listen(3000, function () {
-  console.log('Giphy listening on port localhost:3000!');
-});
+  console.log('Giph App listening on port 3000!')
+})
