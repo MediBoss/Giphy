10a50125924346b6743ce9ad1245c1288ab4cb30
Medi Assumani <mediassumani49@gmail.com>
2019-01-04 22:07:05 -0800

    practicing advanced git concepts

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var http = require('http');
-
-var app = express(); //initializes a new express object
+var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -13,7 +12,7 @@ app.use(express.static('public'));
 
   // The root route
 app.get('/', function (request, res) {
-  var queryString = request.query.term;
+  var queryString = request.query.term
   var term = encodeURIComponent(queryString); // encodes the search term requested
   var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
 
@@ -30,11 +29,12 @@ app.get('/', function (request, res) {
       // Data reception is done, do whatever with it!
       var parsedDataFromAPI = JSON.parse(dataToBeReturned); // parses the JSON response data into desirable type(giphs in this case)
       res.render('home', {gifs: parsedDataFromAPI.data}) // updates the view
+
     });
   });
 
 })
 
 app.listen(3000, function () {
-  console.log('Giph App listening on port 3000!')
+  console.log('Server Listening on Port 3000')
 })
